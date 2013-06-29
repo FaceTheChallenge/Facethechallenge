22d837be25af72edda7bf7e0f495be5feb46955c
Manu Vargas Plata <charnuel@hotmail.com>
2013-06-29 11:58:45 +0200

    Comprobacion campos formulario registro.
    Añadida funcionalidad completa de comprobación de campos en el
    formulario de registro.
    
    Se comprueba todo:
    Nombre
    Usuario
    Correo
    Clave y repeticion de clave
    Fecha de nacimiento
    Movil
    Genero (Por defecto masculino)
    
    Ya no se registrará un usuario si no está todo relleno.

--- public/js/comprobar.js
+++ public/js/comprobar.js
@@ -16,23 +16,78 @@ function comprobar()
    return true;
 }
 
-	function obtenValores(){
-	//Voy a ir recogiendo todos los valores del campo para enviarlos como una cadena JSON
+function obtenValores(){
+	//Voy a ir recogiendo todos los valores del campo para enviarlos como una cadena JSON, comprobando que todos los campos están rellenos:
+	var envia = 0;
+	
+	//Compruebo el nombre
+	if (document.getElementById('name').value == ""){
+		alert('Debes poner tu nombre');
+		envia = 0;
+		document.getElementById('name').focus();
+		return false;
+		}
+	else{
 		var nombre = document.getElementById('name').value;
+		envia = 1;
+	}
+	
+	//Compruebo el e-mail
+	if (document.getElementById('email').value == ""){
+		alert('Debes poner tu dirección de correo')
+		envia = 0;
+		document.getElementById('email').focus();
+		return false;
+	}	
+	else{
 		var email = document.getElementById('email').value;
+		envia = 1;
+	}
+	
+	//Compruebo el nombre de usuario
+	if (document.getElementById('username').value == ""){
+		alert('Debes elegir un nombre de usuario');
+		document.getElementById('username').focus();
+		envia = 0;
+		return false;
+	}	
+	else{
 		var user = document.getElementById('username').value;
+		envia = 1;
+	}
+	
+	//Compruebo el password
+	if (document.getElementById('password').value == ""){
+		alert('Debes elegir una clave');
+		document.getElementById('password').focus();
+		envia = 0;
+		return false;
+	}	
+	else{
 		var pass = document.getElementById('password').value;
+		envia = 1;
+	}
 		var repass = document.getElementById('repassword').value;
-
+	
 		var sex = document.getElementById('gender').value;
 		//Pongo el sexo del usuario como texto
 		if (sex == "m"){sex = "Masculino";}
 		else{sex = "Femenino";}
 
+	//Compruebo la fecha de nacimiento
+	if (document.getElementById('BirthMonth').value == "" || document.getElementById('BirthDay').value == "" || document.getElementById('BirthYear').value == ""){
+		alert('Fecha de nacimiento incorrecta');
+		document.getElementById('BirthMonth').focus();
+		envia = 0;
+		return false;
+	}	
+	else{
 		var mes = document.getElementById('BirthMonth').value;
 		var dia = document.getElementById('BirthDay').value;
 		var anio = document.getElementById('BirthYear').value;
-		
+		envia = 1;
+	}
+	
 		//Pongo el mes con nombre
 		if (mes == "01"){mes = "Enero";}
 		if (mes == "02"){mes = "Febrero";}
@@ -50,22 +105,31 @@ function comprobar()
 		//Obtengo la fecha de nacimiento completa
 		var nacimiento = dia + "-" + mes + "-" + anio;
 	
+	//Compruebo el telefono movil
+	if (document.getElementById('phone').value == ""){
+		alert('Necesitamos saber tu móvil');
+		document.getElementById('phone').focus();
+		envia = 0;
+		return false;
+	}	
+	else{
 		var movil = document.getElementById('phone').value;
+		envia = 1;
+	}
 
 		var f = new Date();
 		var fechaRegistro = f.getDate() + "-" + (f.getMonth() +1) + "-" + f.getFullYear();
 		
 		//Ahora, para enviar los datos, compruebo por ejemplo que el password está bien repetido:
 		if (pass==repass){
-		//var peticion = "http://localhost:3000/registrar/"+nombre+"/"+sex+"/"+nacimiento+"/"+email+"/"+user+"/"+pass+"/"+fechaRegistro+"/"+movil;
-		var peticion = "/registrar/"+nombre+"/"+sex+"/"+nacimiento+"/"+email+"/"+user+"/"+pass+"/"+fechaRegistro+"/"+movil;
-		request.open('POST',peticion,true);
-		request.onreadystatechange=alert('¡¡Bienvenido!!  ¡¡Eres un valiente!!\n\n Espera mientras te redireccionamos al inicio...');
-		request.send(null);
+			if (envia == 1){
+				var peticion = "/registrar/"+nombre+"/"+sex+"/"+nacimiento+"/"+email+"/"+user+"/"+pass+"/"+fechaRegistro+"/"+movil;
+				request.open('POST',peticion,true);
+				request.onreadystatechange=alert('¡¡Bienvenido!!  ¡¡Eres un valiente!!\n\n Espera mientras te redireccionamos al inicio...');
+				request.send(null);
 		
-		setTimeout('document.location = "/pagInicio"',2000);
-		
-
+				setTimeout('document.location = "/pagInicio"',2000);
+			}
 		}
 		else{alert('Eh!! La clave no coincide!!');}
 		
@@ -86,24 +150,7 @@ function retar() {
 		request.onreadystatechange=alert('Reto creado.\n\n Espera mientras te redirigimos...');
 		request.send(null);
 		
-		setTimeout('document.location = "/pagPrincipal"',10000);
+		setTimeout('document.location = "/pagPrincipal"',2000);
 		}
 		
-		
-		
-	    /*var request = new XMLHttpRequest();
-  		var peticion_str = 'http://localhost:3000/retar/dificultad/'+valor+'/';
-  		request.open('GET', peticion_str , true);
-  		request.onreadystatechange= function(){
-			if ( request.readyState == 4 ) {
-				console.log(request);
-				var json;
-				eval ( 'json = '+ request.responseText );
-				console.log(json);
-  			}	
-		};
-  		request.send(null);*/
-		
 }
-
-// JavaScript Document
